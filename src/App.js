f10fcb5ba8775d7154352315c6e5886c91828885
Siddharth Pachori <siddpachori@gmail.com>
2019-06-22 13:46:32 +0530

    MY FIRST COMMIT

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyFirstComponent from './components/MyFirstComponent'
+
+function App() {
+  return (
+    <div className="App">
+       <MyFirstComponent></MyFirstComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class MyFirstComponent {
+    render() {
+        return (
+            <div>I am first component</div>
+        )
+    }
+}
